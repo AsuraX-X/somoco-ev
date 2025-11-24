d287fc76c9daac56732811bc3e67a419b83a2b4e
asura <kiba741@gmail.com>
2025-11-24 16:57:10 +0000

    Refactor button to use Link component for navigation to products

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -4,6 +4,7 @@ import Hero from "@/components/Home/Hero";
 import Ready from "@/components/Home/Ready";
 import Why from "@/components/Home/Why";
 import { motion } from "motion/react";
+import Link from "next/link";
 
 // export const metadata = {
 //   title: "SOMOCO EV — Home",
@@ -17,18 +18,20 @@ const Home = () => {
       <div>
         <Hero />
         <div className="w-screen absolute h-screen left-0 top-[74px] flex items-end justify-center">
-          <motion.button
-            initial={{ y: 50, opacity: 0, background: "#00101466" }}
-            animate={{ y: 0, opacity: 1 }}
-            whileHover={{
-              backgroundColor: "#001014",
-              transition: { duration: 0.3 },
-            }}
-            transition={{ ease: "easeInOut" }}
-            className="border-secondary mb-40  backdrop-blur-xs text-2xl z-1 shadow-2xs cursor-pointer border rounded-full px-8 py-2"
-          >
-            Find Your Car
-          </motion.button>
+          <Link href={"/products"}>
+            <motion.button
+              initial={{ y: 50, opacity: 0, background: "#00101466" }}
+              animate={{ y: 0, opacity: 1 }}
+              whileHover={{
+                backgroundColor: "#001014",
+                transition: { duration: 0.3 },
+              }}
+              transition={{ ease: "easeInOut" }}
+              className="border-secondary mb-40  backdrop-blur-xs text-2xl z-1 shadow-2xs cursor-pointer border rounded-full px-8 py-2"
+            >
+              Find Your Car
+            </motion.button>
+          </Link>
         </div>
         <Find />
       </div>
